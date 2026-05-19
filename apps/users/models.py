7aee42ae2869a8f77d49309d5b0cf34f7eb78a83
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-18 21:58:06 -0700

    add user model to models.py

---
+++ apps/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    pass
+    # We use AbstractUser to future-proof the core relationships,
+    # and keep all other generic account fields on the Profile.
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, max_length=500)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    is_public = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
