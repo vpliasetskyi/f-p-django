a14805725c83ed5dcc597e33d105d4a8ba7deb1c
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-18 21:53:30 -0700

    modify setting.py: add apps.users to apps list,define auth user model

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.users",
 ]
+
+AUTH_USER_MODEL = "users.CustomUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
