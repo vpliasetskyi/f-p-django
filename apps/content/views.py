b7d9e98f109cf9773454636fd858ffac3cb251af
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-21 03:58:10 -0700

    create content/urls.py and modify views.py

--- apps/content/views.py
+++ apps/content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import ContentItem
 
-# Create your views here.
+def home_view(request):
+    # Fetch the 12 most recently added content items
+    recent_items = ContentItem.objects.order_by('-created_at')[:12]
+    
+    context = {
+        'recent_items': recent_items,
+    }
+    return render(request, "content/home.html", context)
