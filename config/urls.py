43254dff6df6bfd29e471a5ef858dfff1272987f
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-22 11:22:12 -0700

    update config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.users.urls")),
     path("lists/", include("apps.lists.urls")),
     path("", include("apps.content.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
