8ff1473332d1976df31d339d2cb1b9213f3ec8e5
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-22 11:21:36 -0700

    add profile update form and view

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
+from .models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -10,3 +12,9 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_picture', 'is_public']
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("profile/edit/", views.ProfileUpdateView.as_view(), name="profile_edit"),
+    path("profile/<str:username>/", views.ProfileDetailView.as_view(), name="profile"),
 ]
