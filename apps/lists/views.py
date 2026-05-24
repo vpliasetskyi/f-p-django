99530c3244eb8feb1d10b1fd6efc539dfde74acc
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-24 05:15:32 -0700

    update lists/views

--- apps/lists/views.py
+++ apps/lists/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import ListView, View
+from django.views.generic import ListView, View, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import get_user_model
-from .models import WatchItem
+from django.urls import reverse_lazy
+from .models import WatchItem, CustomList
 from apps.content.models import ContentItem
 
 class UserWatchListView(LoginRequiredMixin, ListView):
@@ -37,9 +38,68 @@ class WatchItemToggleView(LoginRequiredMixin, View):
 class CloneWatchListView(LoginRequiredMixin, View):
     def post(self, request, username):
         target_user = get_object_or_404(get_user_model(), username=username)
-        
+
         if not target_user.profile.is_public:
             return redirect('profile', username=username)
-            
+
         WatchItem.objects.clone_list_to_user(target_user, request.user)
         return redirect('lists:my_list')
+
+
+class CustomListCreateView(LoginRequiredMixin, CreateView):
+    model = CustomList
+    fields = ['name', 'description']
+    template_name = 'lists/custom_list_form.html'
+    success_url = reverse_lazy('lists:my_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CustomListUpdateView(LoginRequiredMixin, UpdateView):
+    model = CustomList
+    fields = ['name', 'description']
+    template_name = 'lists/custom_list_form.html'
+    success_url = reverse_lazy('lists:my_list')
+
+    def get_queryset(self):
+        return CustomList.objects.filter(user=self.request.user)
+
+
+class CustomListDeleteView(LoginRequiredMixin, DeleteView):
+    model = CustomList
+    success_url = reverse_lazy('lists:my_list')
+
+    def get_queryset(self):
+        return CustomList.objects.filter(user=self.request.user)
+
+
+class AddToListView(LoginRequiredMixin, View):
+    def post(self, request, content_id):
+        content_item = get_object_or_404(ContentItem, id=content_id)
+        list_id = request.POST.get('list_id')
+        list_name = request.POST.get('list_name', '').strip()
+
+        if list_id:
+            custom_list = get_object_or_404(CustomList, id=list_id, user=request.user)
+        elif list_name:
+            custom_list, _ = CustomList.objects.get_or_create(user=request.user, name=list_name)
+        else:
+            return render(request, 'lists/partials/add_to_list_result.html', {'error': 'No list specified.'})
+
+        custom_list.items.add(content_item)
+        user_lists = CustomList.objects.filter(user=request.user).order_by('name')
+        return render(request, 'lists/partials/add_to_list_result.html', {
+            'custom_list': custom_list,
+            'content_item': content_item,
+            'user_lists': user_lists,
+        })
+
+    def get(self, request, content_id):
+        content_item = get_object_or_404(ContentItem, id=content_id)
+        user_lists = CustomList.objects.filter(user=request.user).order_by('name')
+        return render(request, 'lists/partials/add_to_list_panel.html', {
+            'content_item': content_item,
+            'user_lists': user_lists,
+        })
