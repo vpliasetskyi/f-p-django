a8f571dd14668ae6dd0ec9bf51d758adf80757ac
vpliasetskyi <v.pliasetskyi@gmail.com>
2026-05-24 05:07:58 -0700

    update view of content app add AdvanceSearchView

--- apps/content/views.py
+++ apps/content/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, DetailView
-from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView, View
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from datetime import datetime
 from .models import ContentItem
@@ -63,8 +63,15 @@ class ContentDetailView(DetailView):
                 contnt_type=content_type,
                 release_date=release_date,
                 overview=data.get('overview', ''),
-                poster_path=data.get('poster_path', '')
+                poster_path=data.get('poster_path', ''),
+                vote_average=data.get('vote_average'),
             )
+        else:
+            # Refresh vote_average from TMDB if stale
+            vote_average = data.get('vote_average') if (data := tmdb.get_details(item.tmdb_id, item.contnt_type)) else None
+            if vote_average is not None and vote_average != item.vote_average:
+                item.vote_average = vote_average
+                item.save(update_fields=['vote_average'])
             
         return item
         
@@ -74,3 +81,28 @@ class ContentDetailView(DetailView):
             from apps.lists.models import WatchItem
             context['watch_item'] = WatchItem.objects.filter(user=self.request.user, contnt_item=self.object).first()
         return context
+
+
+class AdvancedSearchView(View):
+    def get(self, request):
+        genres_data = tmdb.get_genres()
+        all_genres = {g['id']: g['name'] for g in genres_data.get('movie', []) + genres_data.get('tv', [])}
+        unique_genres = [{'id': k, 'name': v} for k, v in sorted(all_genres.items(), key=lambda x: x[1])]
+        context = {'genres': unique_genres, 'results': []}
+        return render(request, 'content/advanced_search.html', context)
+
+    def post(self, request):
+        media_type = request.POST.get('media_type', 'movie')
+        year = request.POST.get('year') or None
+        country = request.POST.get('country') or None
+        genre_id = request.POST.get('genre_id') or None
+        min_rating = request.POST.get('min_rating') or None
+
+        results = tmdb.discover_media(
+            media_type=media_type,
+            year=year,
+            country=country,
+            genre_id=genre_id,
+            min_rating=min_rating,
+        )
+        return render(request, 'content/partials/discover_results.html', {'results': results})
